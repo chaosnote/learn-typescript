382bbae4edcf36aeaaafcbbdd65a12f9fbf947f5
chris <yeditor@gmail.com>
2024-11-04 13:55:25 +0800

    1、刪除不必要的輸出

--- command/start.js
+++ command/start.js
@@ -48,3 +48,46 @@ try {
 } catch (error) {
     console.error(`${error.message}`);
 }
+
+/**
+ * 刪除資料夾並排除指定的檔案
+ * @param {string} dirPath - 要刪除的資料夾路徑
+ * @param {Object} excludeFiles - 要排除的檔案 (關聯陣列)
+ */
+async function deleteFolderWithExclusions(dirPath, excludeFiles) {
+    try {
+        const files = await fs.promises.readdir(dirPath);
+
+        for (const file of files) {
+            const filePath = path.join(dirPath, file);
+            const stat = await fs.promises.stat(filePath);
+
+            // 如果是資料夾，進行遞迴刪除
+            if (stat.isDirectory()) {
+                await deleteFolderWithExclusions(filePath, excludeFiles);
+            } else {
+                // console.log(filePath) ;
+                // 如果是檔案且不在排除清單中，刪除它
+                if (!excludeFiles[filePath]) {
+                    await fs.promises.unlink(filePath);
+                }
+            }
+        }
+
+        // 刪除空資料夾（如果資料夾中的檔案已經全部刪除）
+        const remainingFiles = await fs.promises.readdir(dirPath);
+        if (remainingFiles.length === 0) {
+            await fs.promises.rmdir(dirPath);
+        }
+    } catch (error) {
+        console.error(`Error while deleting folder: ${error.message}`);
+    }
+}
+
+const excludeFiles = {
+    "dist\\kernel-0.0.0-development.tgz": true,
+};
+
+deleteFolderWithExclusions('./dist', excludeFiles)
+    .then(() => console.log('指定資料夾已成功刪除並排除特定檔案'))
+    .catch(console.error);
